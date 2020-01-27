361293e41c185de4f6d7c61c7ee7521097e6d5c1
nagaraju <naga.thota99@gmail.com>
2020-01-27 15:01:20 +0530

    Initial Commit

---
+++ SystemTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SystemTask
+//
+//  Created by THOTA NAGARAJU on 1/27/20.
+//  Copyright © 2020 THOTA NAGARAJU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
